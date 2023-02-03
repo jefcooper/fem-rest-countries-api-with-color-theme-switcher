2b0cf531df3f645631bc26f04391a12da687730b
Jeff Cooper <jefcooper@gmail.com>
2023-02-03 20:38:51 +0000

    api change to use a project file

--- api/regions/index.js
+++ api/regions/index.js
@@ -1,7 +1,7 @@
 import { promises as fs } from "fs";
 
 export default async (request, response) => {
-  const data = await fs.readFile("./api/data/regions.json", "utf8");
+  const data = await fs.readFile("data/regions.json", "utf8");
   const regions = JSON.parse(data);
 
   response.status(200).json(regions);
