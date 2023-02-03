6d0a5ea136070e03ea03072eca3c7d45a78a9187
Jeff Cooper <jefcooper@gmail.com>
2023-02-03 20:42:03 +0000

    use cwd

--- api/regions/index.js
+++ api/regions/index.js
@@ -1,7 +1,9 @@
 import { promises as fs } from "fs";
+import { path } from "path";
 
 export default async (request, response) => {
-  const data = await fs.readFile("data/regions.json", "utf8");
+  const dataDir = path.join(process.cwd(), "data");
+  const data = await fs.readFile(dataDir + "/regions.json", "utf8");
   const regions = JSON.parse(data);
 
   response.status(200).json(regions);
