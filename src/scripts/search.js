2fd8cd2a84865d7ca79a7bf30c9106f38ca70615
Jeff Cooper <jefcooper@gmail.com>
2023-02-06 11:13:27 +0000

    update browser history for navigation using pushstate

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -4,24 +4,41 @@
 import element from "./element-factory";
 
 //
+// elements from web page
+//
 const searchInput = document.querySelector("[data-search-input]");
 const regionSelect = document.querySelector("[data-region-select]");
 const searchResults = document.querySelector("[data-search-results]");
+
+//
+// state variables
+//
 let searchFilter;
 let regions;
+let timer;
+let countryIndexData;
 
 console.log("initializing searchInput");
 
-const filterRegion = new URLSearchParams(location.search).get("region");
+// obtain current URL search parameter if present at page load
+let filterRegion = new URLSearchParams(location.search).get("region");
 
-if (searchInput) {
-  regionSearch();
-  performSearch();
+// perform page-load initialization
+initializeSearchLibrary();
+
+/**
+ * First time initialization on page load
+ */
+function initializeSearchLibrary() {
+  if (searchInput) {
+    regionSearch();
+    performSearch();
+  }
 }
 
-let timer;
-
-// start simple with enter and click handler
+//
+// search input keyboard event listener
+//
 searchInput?.addEventListener("keydown", async function (evt) {
   if (evt.key === "Enter") {
     performSearch();
@@ -38,49 +55,34 @@ searchInput?.addEventListener("keydown", async function (evt) {
   }
 });
 
+//
+// region select dropdown change event listener
+//
+regionSelect?.addEventListener("change", (evt) => {
+  const updatedUrl =
+    location.origin + location.pathname + "?region=" + evt.target.value;
+  history.pushState({}, null, updatedUrl);
+  history.scrollRestoration = "manual";
+  updateRegionFilter(evt.target.value);
+  performSearch();
+});
+
+window.addEventListener("popstate", (evt) => {
+  const newRegion = new URLSearchParams(location.search).get("region");
+  regionSelect.value = newRegion;
+  updateRegionFilter(newRegion);
+  performSearch();
+});
+/**
+ * performSearch - search based on region filter and text input in search box
+ */
 async function performSearch() {
   const result = await countrySearch(searchInput.value);
   const filteredResult = result
     .filter((val) => !searchFilter || searchFilter.has(val[1]))
     .sort((a, b) => a[0].localeCompare(b[0]));
-  fillSearchResults(filteredResult);
+  fillSearchResults(filteredResult, await countryIndex());
 }
-
-async function fillSearchResults(result) {
-  searchResults.innerText = "";
-  const index = await countryIndex();
-  result.forEach((country) => {
-    const liEl = element("li").class("card").addTo(searchResults);
-    const aEl = element("a")
-      .attribute("href", "/country.html?country=" + country[1])
-      .addTo(liEl);
-    element("img")
-      .class("card__flag")
-      .attribute("src", index[country[1]].flag)
-      .attribute("loading", "lazy")
-      .addTo(aEl);
-    const cardInfoEl = element("div").class("card__info").addTo(aEl);
-    element("h2").text(country[0]).class("heading--2").addTo(cardInfoEl);
-    const dl = element("dl").addTo(cardInfoEl);
-    element("dt").text("Population").class("label--1").addTo(dl);
-    element("dd")
-      .text(Number.parseInt(index[country[1]].population, 10).toLocaleString())
-      .class("text--1")
-      .addTo(dl);
-    element("dt").text("Region").class("label--1").addTo(dl);
-    element("dd").text(index[country[1]].region).class("text--1").addTo(dl);
-    element("dt").text("Capital").class("label--1").addTo(dl);
-    element("dd")
-      .text(index[country[1]].capital || "none")
-      .class("text--1")
-      .addTo(dl);
-  });
-}
-
-regionSelect?.addEventListener("change", (evt) => {
-  updateRegionFilter(evt.target.value);
-  location.assign("/index.html?region=" + evt.target.value);
-});
 
 function updateRegionFilter(regionName) {
   if (!regionName || regionName === "all") {
@@ -99,8 +101,6 @@ async function countrySearch(name) {
     return data;
   }
 }
-
-let countryIndexData;
 
 async function countryIndex() {
   if (!countryIndexData) {
@@ -140,4 +140,45 @@ function initializeRegionOptions(data) {
     regionSelect.value = filterRegion;
     updateRegionFilter(filterRegion);
   }
+}
+
+/**
+ * fillSearchResults - dynamically create the results section of the page
+ */
+function fillSearchResults(result, countryIndex) {
+  searchResults.innerText = "";
+  result.forEach((country) => {
+    const liEl = element("li").class("card").addTo(searchResults);
+    const aEl = element("a")
+      .attribute("href", "/country.html?country=" + country[1])
+      .addTo(liEl);
+    element("img")
+      .class("card__flag")
+      .attribute("src", countryIndex[country[1]].flag)
+      .attribute("loading", "lazy")
+      .addTo(aEl);
+    const cardInfoEl = element("div").class("card__info").addTo(aEl);
+    element("h2").text(country[0]).class("heading--2").addTo(cardInfoEl);
+    const dl = element("dl").addTo(cardInfoEl);
+    element("dt").text("Population").class("label--1").addTo(dl);
+    element("dd")
+      .text(
+        Number.parseInt(
+          countryIndex[country[1]].population,
+          10
+        ).toLocaleString()
+      )
+      .class("text--1")
+      .addTo(dl);
+    element("dt").text("Region").class("label--1").addTo(dl);
+    element("dd")
+      .text(countryIndex[country[1]].region)
+      .class("text--1")
+      .addTo(dl);
+    element("dt").text("Capital").class("label--1").addTo(dl);
+    element("dd")
+      .text(countryIndex[country[1]].capital || "none")
+      .class("text--1")
+      .addTo(dl);
+  });
 }
